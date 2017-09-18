f548e1eeb4154d752283e900fb58eb8443e98420
olddonkey <olddonkeyblog@gmail.com>
2017-09-18 00:19:28 -0500

    Initial Commit

---
+++ Ticker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ticker
+//
+//  Created by olddonkey on 2017/9/18.
+//  Copyright © 2017年 olddonkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
