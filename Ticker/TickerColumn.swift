b8e8cb7a2f0d21c1180801da57d17bd7144691a5
olddonkey <olddonkeyblog@gmail.com>
2017-10-08 01:38:01 -0500

    Almost finish column

--- Ticker/TickerColumn.swift
+++ Ticker/TickerColumn.swift
@@ -18,8 +18,8 @@ class TickerColumn: UIView {
     }
     */
     
-    static let unknown_start_index = -1
-    static let unknown_end_index = -2
+    let unknown_start_index = -1
+    let unknown_end_index = -2
     
     var characterList: [Character] = [Character]()
     var characterIndicesMap: [Character: Int] = [Character: Int]()
@@ -62,11 +62,90 @@ class TickerColumn: UIView {
         self.targetWidth = metrics.getCharWidth(character: targetChar)
         self.minimumRequiredWidth = max(self.sourceWidth, self.targetWidth)
         
+        setCharacterIndice()
         
+        let scrollDown = endIndex >= startIndex
+        directionAdjustment = scrollDown ? 1 : -1
+        
+        previousBottomDelta = currentBottomDelta
+        currentBottomDelta = 0
+    }
+    
+    func getCurrentChar() -> Character {
+        return currentChar
+    }
+    
+    func getTargetChar() -> Character {
+        return targetChar
+    }
+    
+    func getCurrentWidth() -> CGFloat {
+        return currentWidth
+    }
+    
+    func getMinimunRequiredWidth() -> CGFloat {
+        return minimumRequiredWidth
     }
     
     func setCharacterIndice() {
+        startIndex = characterIndicesMap[currentChar] ?? unknown_start_index
+        endIndex = characterIndicesMap[targetChar] ?? unknown_end_index
+    }
+    
+    func onAnimationEnd() {
+        minimumRequiredWidth = currentWidth
+    }
+    
+    func setAnimationProgress(animationProgress: CGFloat) {
+        /// If animate to the end, then directly set the value
+        if animationProgress == 1 {
+            currentChar = targetChar
+            currentBottomDelta = 0
+            previousBottomDelta = 0
+        }
         
+        let charHeight = metrics.getCharHeight()
+        let totalHeight = charHeight*CGFloat(abs(endIndex-startIndex))
+        
+        let currentBase = animationProgress*totalHeight
+        
+        let bottomCharPosition = currentBase/charHeight
+        
+        let bottomCharOffsetPercentage = bottomCharPosition - bottomCharPosition
+        
+        let additionalDelta = previousBottomDelta * (1 - animationProgress)
+        
+        bottomDelta = bottomCharOffsetPercentage * charHeight * directionAdjustment + additionalDelta
+        
+        bottomCharIndex = startIndex + Int(bottomCharPosition * directionAdjustment)
+        
+        self.charHeight = charHeight
+        self.currentWidth = sourceWidth +  (targetWidth - sourceWidth) * animationProgress
+    }
+    
+    func drawColumn() {
+        if drawText(characterList: characterList, index: bottomCharIndex, verticalOffset: bottomDelta) {
+            if bottomCharIndex >= 0 {
+                currentChar = characterList[bottomCharIndex]
+            }else if bottomCharIndex == unknown_end_index {
+                currentChar = targetChar
+            }
+            currentBottomDelta = bottomDelta
+        }
+    }
+    
+    func drawText(characterList: [Character], index: Int, verticalOffset: CGFloat) -> Bool {
+        if index >= 0 && index < characterList.count {
+            
+            return true
+        }else if startIndex == unknown_start_index && index == unknown_start_index {
+            
+            return true
+        }else if endIndex == unknown_end_index && index == unknown_end_index {
+            
+            return true
+        }
+        return false
     }
     
     
---
+++ Ticker/TickerColumnManager.swift
@@ -0,0 +1,21 @@
+//
+//  TickerColumnManager.swift
+//  Ticker
+//
+//  Created by olddonkey on 2017/10/8.
+//  Copyright © 2017年 olddonkey. All rights reserved.
+//
+
+import UIKit
+
+class TickerColumnManager: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
